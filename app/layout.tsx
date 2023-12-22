6dd035b201e631d910ba87576d3f1a75f116be71
Harsh Sharma <harshsharma20503@gmail.com>
2023-12-22 16:05:33 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Harsh\'s Portfolio',
-  description: 'This is the portfolio of an aspiring SDE',
+  description: 'Explore my portfolio project—a concise compilation offering a comprehensive overview of my diverse skill set and experiences. Through an intuitive interface, it seamlessly presents a spectrum of projects, emphasizing simplicity, functionality, and practicality, providing a nuanced reflection of my professional journey',
 }
 
 export default function RootLayout({
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='h-screen w-screen dark'>
+      <head>
+        <link rel="icon" href="/Initials.png" sizes="any" />
+      </head>
       <body className={`bg-fixed bg-gradient-to-r from-green-800 to-blue-400 dark:from-dark-500 dark:to-dark-300 dark:text-white ${inter.className}`}>
         {children}
       </body>
